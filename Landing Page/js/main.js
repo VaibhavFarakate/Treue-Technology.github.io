515e728a4fe5bec84ca79d3ffe46e5f85d592481
VaibhavFarakate <vaibhav7525@gmail.com>
2023-08-07 19:15:37 +0530

    courses,Footer

--- Landing Page/js/main.js
+++ Landing Page/js/main.js
@@ -1,36 +1,23 @@
-$(".btnToggle").click(toggler);
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
 
-function toggler() {
-  var btnId = $(this).data("id");
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
 
-  var ElementId = "#toggleIcon" + btnId;
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
 
-  if ($(ElementId).hasClass("down")) {
-    $(ElementId).animate({ rotate: "180deg" }, 300);
-    $(ElementId).addClass("up");
-    $(ElementId).removeClass("down");
-  } else if ($(ElementId).hasClass("up")) {
-    $(ElementId).animate({ rotate: "0deg" }, 300);
-    $(ElementId).addClass("down");
-    $(ElementId).removeClass("up");
-  }
-}
-
-// ScrollTop Button function
-let mybutton = document.getElementById("myBtn");
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
